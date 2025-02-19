821f691a49516492dc82ab57cff18ebb2e043518
kuszd24d <kuszd.24d@acsjszki.hu>
2025-02-19 14:09:03 +0100

    reghnggr

---
+++ script.js
@@ -0,0 +1,46 @@
+// kiírja a konzolra a Hello, world! szöveget
+console.log('Hello, world!');
+
+// egysoros komment
+
+/*
+kuki
+muki
+duki
+komment...
+*/
+
+//Adattípusok
+
+//1. szöveg (string)
+console.log('ez egy szöveg típus');
+
+//2. szám (number)
+console.log(69420);
+
+//3. logikai (boolean)
+// két értéke lehet: true vagy false
+console.log(true);
+console.log(false);
+
+//4. undefined
+console.log(undefined);
+
+//5. null
+console.log(null);
+
+//Összetett adattípusok
+
+
+//6. tömb (array)
+console.log([1, 2, 3, 4, 5]);
+
+//7. objektum (object)
+console.log({
+    age: 25,
+    "firstName": "John",
+    "lastName": "Doe",
+});
+
+document.getElementById('cim').style.backgroundColor = 'red';
+document.getElementById('cim').style.border = '2px solid black';
